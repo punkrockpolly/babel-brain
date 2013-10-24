e5abea395bc7d4dbf3d1e1f5d6930dbb62f2c1b1
Polina Giralt <polina.giralt@gmail.com>
2013-10-24 18:20:02 -0400

    work in progress: debugging algorithm that guesses weights

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -49,6 +49,7 @@ def new_feature_weights():
     # build dictionary of all features
     feature_dict = {}
     feature_dict = feat_init_letter_freq(feature_dict)
+    save_knowledge(feature_dict)
     return feature_dict
 
 
@@ -64,17 +65,11 @@ def sentences(filename):
     sentences = open(filename)
     rawinput = sentences.read().lower()
     input_list = rawinput.split("\n")
-
-    num_removed = 0
     new_list = []
     for sentence in input_list:
         stripped = sentence.strip()
         if stripped != "":
             new_list.append(stripped)
-        else:
-            num_removed += 1
-
-    print str(num_removed) + ":" + filename
     return new_list
 
 
@@ -98,7 +93,7 @@ def extract_features(string_input):
     avg_sentance_len = avg_sentance_len(string_input)
     avg_word_len = avg_word_len(string_input)
     features = dict()
-    
+
     features['word_len'] = avg_word_len
     features['sent_len'] = avg_sentance_len
     features['char_freq'] = 0
@@ -135,8 +130,8 @@ def classification_score(feature_values, feature_weights):
     # features values = featurename -> normalizedfeatureValue
     # feature weights  = featurename -> feature weight
     score = 0
-    for this_feature_value in feature_values:
-        score += this_feature_value * feature_weights[this_feature_value]
+    for this_feature_weight in feature_weights:
+        score += feature_values[this_feature_weight] * feature_weights[this_feature_weight]
     return score
 
 
@@ -146,13 +141,13 @@ def get_threshold():
 
 def guess_is_english(sentence):
     # get feature weights
-    feature_weights = get_knowledge() 
-    # feature vals 
+    feature_weights = get_knowledge()
+    # feature vals
     feature_values = sentence_to_normailized_frequency_values_dict(sentence)
-    # calc score 
-    score = classification_score(feature_values,feature_weights)
+    # calc score
+    score = classification_score(feature_values, feature_weights)
     # get threshold
-    threshold = get_threshold 
+    threshold = get_threshold
     # compare score to threshhold
     if score > threshold:
         #print "We think this is English", sentence
@@ -162,34 +157,34 @@ def guess_is_english(sentence):
         return False
 
 
-class CharWeights(object):
+class LanguageBot(object):
     def __init__(self):
         self.english_correct = 0
         self.spanish_correct = 0
-        self.weights = new_feature_weights()
-        self.best_weights = self.weights
+        self.feature_weights = get_knowledge()
+        self.best_weights = self.feature_weights
         self.english_sentences = sentences('english-sentences.txt')
         self.spanish_sentences = sentences('spanish-sentences.txt')
-        self.best_score = self.english_guesses_correct()
+        self.best_score = self.english_guesses_correct() + self.spanish_guesses_correct()
 
     def english_guesses_correct(self):
         self.english_correct = 0
         for sentence in self.english_sentences:
-            if guess_is_english(sentence, self.weights):
+            if guess_is_english(sentence):
                 self.english_correct += 1
         return self.english_correct
 
     def spanish_guesses_correct(self):
         self.spanish_correct = 0
         for sentence in self.spanish_sentences:
-            if not guess_is_english(sentence, self.weights):
+            if not guess_is_english(sentence):
                 self.spanish_correct += 1
         return self.spanish_correct
 
-    def find_best_weights(self):
+    def train(self):
         for letter in string.ascii_lowercase:
             for x in range(-5, 6):
-                self.weights[letter] = x
+                self.feature_weights[letter] = x
                 self.new_score = self.english_guesses_correct() + self.spanish_guesses_correct()
                 if self.new_score > self.best_score:
                     self.best_score = self.new_score
@@ -214,9 +209,9 @@ class CharWeights(object):
 # # now we have optimal weight
 #     languageBot.showWeghts()
 
-Language_Bot = CharWeights()
+Language_Bot = LanguageBot()
 print Language_Bot.best_weights
 print Language_Bot
-Language_Bot.find_best_weights()
+Language_Bot.train()
 print Language_Bot
 print Language_Bot.best_weights
