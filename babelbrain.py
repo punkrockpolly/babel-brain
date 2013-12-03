3ff5a2c35cdec530e3bc3ed2850fbec31f26a555
Polina Giralt <polina.giralt@gmail.com>
2013-12-03 16:03:29 -0500

    updated

--- babelbrain.py
+++ babelbrain.py
@@ -1,3 +1,12 @@
 import string
 import json
 import collections
+
+# inputs: 
+# nn_params: parameters for Theta1 and Theta2, the weight matrices for our 2 layer neural network
+# input_layer_size: used to reshape Thetas
+# hidden_layer_size: used to reshape Thetas
+# num_labels: number of classifiers: used to reshape Thetas
+# X: input matrix
+# y: result vector
+# lambda: regularizatio 
