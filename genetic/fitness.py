3d61fae0b7218ce8bfc25ad9698967cd7f448f09
Polina Giralt <polina.giralt@gmail.com>
2013-12-07 02:44:54 -0500

    moves classification_score into fitness.py todo: finish refactoring, implement cost function and genetic optimization algorithm

--- genetic/fitness.py
+++ genetic/fitness.py
@@ -1,2 +1,7 @@
-def fitness(gene):
-	
+def classification_score(feature_values, feature_weights):
+    # features values = featurename -> normalizedfeatureValue
+    # feature weights  = featurename -> feature weight
+    score = 0
+    for this_feature_weight in feature_weights:
+        score += feature_values[this_feature_weight] * feature_weights[this_feature_weight]
+    return score
