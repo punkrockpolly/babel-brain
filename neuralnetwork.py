87d5c0cecd95797ad62ce462f340070ffda25d59
Polina Giralt <polina.giralt@gmail.com>
2013-12-07 22:33:43 -0500

    refactoring code to modularize all functionality

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,46 +1,24 @@
-import fitness
 import persistence
-import datamodel
+import babelbrain
 
 
-def get_threshold():
-    return 50
+bot = babelbrain.Bot()
 
-
-def guess_is_english(sentence):
-    # get feature weights
-    feature_weights = persistence.get_knowledge()
-    # feature vals
-    feature_values = datamodel.normailized_frequency_values_dict(sentence)
-    # calc score
-    score = fitness.classification_score(feature_values, feature_weights)
-    # get threshold
-    threshold = get_threshold
-    # compare score to threshhold
-    if score > threshold:
-        #print("We think this is English", sentence)
-        return True
-    else:
-        #print("We think this is Spanish", sentence)
-        return False
-
-
-# Language_Bot.update_weights()
+# bot.update_weights()
 
 # for thischar in Chars:
-#     bestWeigt = languageBot.findbestWeight(thisChar)
-#     languageBot.updateWeight(thischar, bestWight)
+#     bestWeigt = bot.findbestWeight(thisChar)
+#     bot.updateWeight(thischar, bestWight)
 
 # # now we have optimal weight
-#     languageBot.showWeights()
+#     bot.showWeights()
 
-Language_Bot = fitness.LanguageBot()
-print(Language_Bot.best_weights)
-print(Language_Bot)
-Language_Bot.train()
-print(Language_Bot)
-print(Language_Bot.best_weights)
+print(bot.best_weights)
+print(bot)
+bot.train()
+print(bot)
+print(bot.best_weights)
 
 userinput = raw_input("Do you want to save new weights data: y/n ")
 if userinput == 'y':
-    persistence.save_knowledge(Language_Bot.best_weights)
+    persistence.save_knowledge(bot.best_weights)
