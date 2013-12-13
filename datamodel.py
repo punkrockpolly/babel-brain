29b563349160577fd566fe46b34b0480906dd742
Polina Giralt <polina.giralt@gmail.com>
2013-12-13 18:21:34 -0500

    WIP: implement feature_normalize() currently not working, needs debugging

--- datamodel.py
+++ datamodel.py
@@ -8,17 +8,17 @@ def sentences(filename):
     sentences = open(filename)
     rawinput = sentences.read()
     clean_string = string_sanitize(rawinput)
-    input_list = clean_string.split("\n")
+    input_list = clean_string.split('\n')
     new_list = []
     for sentence in input_list:
         stripped = sentence.strip()
-        if stripped != "":
+        if stripped != '':
             new_list.append(stripped)
     return new_list
 
 
 def vectorize_results(num_english, num_spanish):
-    return [1 for x in range(num_english)] + [0 for x in range(num_spanish)]
+    return [1.0 for x in range(num_english)] + [0.0 for x in range(num_spanish)]
 
 
 def string_sanitize(string_input):
@@ -76,33 +76,35 @@ def rand_init_ft_weights(L_in, L_out, epsilon_init=0.12):
     return W
 
 
-# Refactor to fix normalize_values_dict - DOES NOT WORK
-def normalize_values_dict(feature_dict):
-    feature_vector = dict_to_vector(feature_dict)
-    # returns a normalized version of X where the mean value of
+# Refactor to fix feature_normalize - DOES NOT WORK
+def feature_normalize(features):
+    # returns a normalized version of features list where the mean value of
     # each feature is 0 and the standard deviation is 1
-    X_norm = feature_vector
-    num_features = feature_vector.length()
-    mu = np.zeros(1, num_features)
-    sigma = np.zeros(1, num_features)
+    features_norm = features
+    num_features = len(features[0])
+    # print('num features: {0}'.format(num_features))
+    mu = np.zeros((1, num_features))
+    # mu = np.zeros(num_features, 1)
+    sigma = np.zeros((1, num_features))
 
-    # for i in range(1, num_features):
-    #     mu[i] = np.mean(X(:, i))
-    #     X_norm[:, i] = X(:, i) - mu(i)
-    #     sigma[i] = np.std(X(:, i))
-    #     X_norm[:, i] = X_norm(:, i) / sigma(i)
+    for i in range(0, num_features):
+        print("i:", i)
+        mu[i] = np.mean(features[:, i])
+        features_norm[:, i] = features[:, i] - mu(i)
+        sigma[i] = np.std(features[:, i])
+        features_norm[:, i] = features_norm[:, i] / sigma(i)
 
-    normalization_dict = {}
-    normalization_dict['X_norm'] = X_norm
-    normalization_dict['mu'] = mu
-    normalization_dict['sigma'] = sigma
+    normalized_dict = {}
+    normalized_dict['features_norm'] = features_norm
+    normalized_dict['mu'] = mu
+    normalized_dict['sigma'] = sigma
 
-    return normalization_dict
+    return normalized_dict
 
 
 def avg_word_len(string_input):
     words = string_input.count(' ') + 1
-    total_chars_in_words = len(string_input.replace(" ", ""))
+    total_chars_in_words = len(string_input.replace(' ', ''))
     return float(total_chars_in_words) / words
 
 
@@ -153,58 +155,62 @@ def sentence_length_normalized_score(sentence, score):
 # Unit Tests for functions
 
 
-def test_dict_to_vector(d):
-    print("Test passed: test_dict_to_vector")
+def test_dict_to_vector():
+    d = {}
+    d[1] = 11
+    d[2] = 22
+    d[3] = 33
+    d[4] = 44
+    print('Test passed: test_dict_to_vector')
     return dict_to_vector(d)
 
 
-def test_extract_features(s):
-    print("\n'{0}' contains the following features: \n{1} \nvector representation: {2}"
+def test_extract_features():
+    s = 'test #~`^&+-=]p\|...string number 23984!!534987'
+    print('\n"{0}" contains the following features: \n{1} \nvector representation: {2}'
           .format(s, extract_features(s), dict_to_vector(extract_features(s))))
-    print("\nnumber of features: {0}\n".format(len(dict_to_vector(extract_features(s)))))
+    print('\nnumber of features: {0}\n'.format(len(dict_to_vector(extract_features(s)))))
 
 
 def test_ft_rand_init(rows=2, cols=3):
-    print("Test passed: ft_rand_init")
+    print('Test passed: ft_rand_init')
     return rand_init_ft_weights(rows, cols)
 
 
-def test_build_training_examples(training_data):
-    output = build_training_examples(training_data)
+def test_build_training_examples():
+    testsentences = ['r14534ishitis a good boy',
+                     'po is a good girl',
+                     'this $dskdj',
+                     '*&^&^&$^#~~!@@H']
+    output = build_training_examples(testsentences)
     print('len of output: {0}\n# of features: {1}'.format(len(output),
                                                           len(output[0])))
     print output
+    return output
 
 
-def test_reshape_thetas(vector):
+def test_reshape_thetas():
+    vector = np.arange(16)
     nn_params = vector
     input_layer_size = 2
     hidden_layer_size = 5
     num_labels = 1
     Theta1, Theta2 = reshape_thetas(nn_params, input_layer_size, hidden_layer_size, num_labels)
-    print('Theta1: {0}\nTheta2: {1}'.format(Theta1, Theta2))
+    print('vector: {0}\nTheta1: {1}\nTheta2: {2}'.format(vector, Theta1, Theta2))
+
+
+def test_feature_normalize():
+    test_data = test_build_training_examples()
+    print(feature_normalize(test_data))
+
 
 m = [[-1, 0, 1], [2, 3, 4], [5, 6, 7]]
-vector = np.arange(16)
 
-d = {}
-d[1] = 11
-d[2] = 22
-d[3] = 33
-d[4] = 44
 
-s = "test #~`^&+-=]p\|...string number 23984!!534987"
-
-testsentences = [s,
-                 'r14534ishitis a good boy',
-                 'po is a good girl',
-                 'this $dskdj',
-                 '*&^&^&$^#~~!@@H']
-# d2 = test_ft_rand_init(5, 1)
-# print(d2)
-# test_dict_to_vector(d)
-# test_extract_features(s)
-# test_build_training_examples(testsentences)
+# print(test_ft_rand_init(5, 1))
+# test_dict_to_vector()
+# test_extract_features()
+# test_build_training_examples()
 # print(unroll(m))
-# print(vector)
-# test_reshape_thetas(vector)
+# test_reshape_thetas()
+# test_feature_normalize()
